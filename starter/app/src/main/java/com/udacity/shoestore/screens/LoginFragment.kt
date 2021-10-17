1df2e949c6894d292bd04332641c8383f6a4a66d
utkarshgupta4jan <72812769+utkarshgupta4jan@users.noreply.github.com>
2021-10-17 16:40:56 +0530

    nav Host Fragment Added

--- starter/app/src/main/java/com/udacity/shoestore/screens/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/LoginFragment.kt
@@ -1,5 +1,24 @@
 package com.udacity.shoestore.screens
 
-class LoginFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment  : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+      val binding : FragmentLoginBinding = DataBindingUtil.inflate(inflater,
+      R.layout.fragment_login,container,false)
+        return  binding.root
+    }
 
 }
