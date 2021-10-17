90be0185f2fc0757ea2331cc344e3d3723d6a349
utkarshgupta4jan <72812769+utkarshgupta4jan@users.noreply.github.com>
2021-10-17 17:32:52 +0530

    implemented navigation for Login Screen SignUp & Login Button take user to Welcome Screen

--- starter/app/src/main/java/com/udacity/shoestore/screens/LoginFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/LoginFragment.kt
@@ -6,19 +6,32 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment  : Fragment() {
 
+    private lateinit var binding: FragmentLoginBinding
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-      val binding : FragmentLoginBinding = DataBindingUtil.inflate(inflater,
+        binding= DataBindingUtil.inflate(inflater,
       R.layout.fragment_login,container,false)
         return  binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.loginButton.setOnClickListener {
+            Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_welcomeFragment)
+        }
+        binding.signupButton.setOnClickListener {
+         //   Navigation.createNavigateOnClickListener(R.id.action_loginFragment_to_welcomeFragment)
+            Navigation.findNavController(it).navigate(R.id.action_loginFragment_to_welcomeFragment)
+        }
     }
 
 }
---
+++ starter/app/src/main/java/com/udacity/shoestore/screens/WelcomeFragment.kt
@@ -0,0 +1,7 @@
+package com.udacity.shoestore.screens
+
+import androidx.fragment.app.Fragment
+
+class WelcomeFragment : Fragment() {
+
+}
