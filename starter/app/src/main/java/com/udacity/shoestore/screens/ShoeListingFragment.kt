1e3861584248f77ad66bc8b232181aaa76363421
utkarshgupta4jan <72812769+utkarshgupta4jan@users.noreply.github.com>
2021-10-18 16:01:36 +0530

    Error while setting up action bar

--- starter/app/src/main/java/com/udacity/shoestore/screens/ShoeListingFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/screens/ShoeListingFragment.kt
@@ -6,21 +6,29 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoelistingBinding
+import com.udacity.shoestore.models.ShoeViewModel
 
 class ShoeListingFragment : Fragment() {
 
-    lateinit var binding: FragmentShoelistingBinding
-
+    private lateinit var binding: FragmentShoelistingBinding
+    private lateinit var viewModel: ShoeViewModel
             override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+                //inflating layout
         binding = DataBindingUtil.inflate(inflater,
         R.layout.fragment_shoelisting,
         container,false)
+
+                viewModel = ViewModelProvider(this).get(ShoeViewModel::class.java)
+
+
+
         return binding.root
             }
 
